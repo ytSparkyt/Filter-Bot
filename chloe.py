b03ceef1daec77c6f969f62297dcb2ee771b346f
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-10-20 21:24:16 -0500

    Update chloe.py

--- chloe.py
+++ chloe.py
@@ -7,5 +7,9 @@ client = commands.Bot(command_prefix="?")
 @client.event
 async def on_ready():
     print("Chloes Ready")
+    
+@client.command()
+async def on():
+    await client.say("I am online on heroku! Wrong, and Savage")
 
 client.run(os.environ.get('BOT_TOKEN'))
